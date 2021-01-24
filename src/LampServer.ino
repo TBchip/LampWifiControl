98345c0096f6ad26cd72c127d0334d9dc5e4d442
TBchip <bischoffthijs@gmail.com>
2021-01-24 10:14:27 +0100

    uploaded server file

---
+++ src/LampServer.ino
@@ -0,0 +1,73 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+const char* ssid = "Bischoff";
+const char* password = "DF2A3F42993C";
+
+ESP8266WebServer server(80);
+
+void turnOn() {
+  server.send(200, "text/plain", "the led should turn on");
+  digitalWrite(13, HIGH);
+}
+void turnOff() {
+  server.send(200, "text/plain", "the led should turn off");
+  digitalWrite(13, LOW);
+}
+
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
+
+void setup(void) {
+  pinMode(13, OUTPUT);
+  
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/on", turnOn);
+  server.on("/off", turnOff);
+
+  server.on("/", []() {
+    server.send(200, "text/plain", "/on (turn led on)\n/off (turn led off)");
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void) {
+  server.handleClient();
+  MDNS.update();
+}
